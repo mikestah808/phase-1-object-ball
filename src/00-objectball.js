2574363b9f7ffcb9426d4ca680d13cd824f6e2b4
Michael Stafford <michaelstafford808@gmail.com>
2021-09-23 11:49:51 -1000

    Michael: completed creating Home and away objects along with keys within home and away objects

---
+++ src/00-objectball.js
@@ -0,0 +1,37 @@
+function gameObject(){
+    {
+        homeObject: {
+            teamName : "Brooklyn Nets",
+            colors : ["Black", "White"],
+            players : {
+                name : "Alan Anderson",
+                number : 0,
+                shoe : 16,
+                points : 22,
+                rebounds : 12,
+                assists : 12,
+                steals : 3,
+                blocks : 1,
+                slamDunks : 1,
+            },
+        },
+        awayObject: {
+            teamName : "Charlotte Hornets",
+            colors : ["Turquoise", "Purple"],
+            players : {
+                name : "Jeff Adrien",
+                number : 4,
+                shoe : 18,
+                points : 10,
+                rebounds : 1,
+                assists : 1,
+                steals : 2,
+                blocks : 7,
+                slamDunks : 2,
+
+            },
+        },
+    };
+}
+
+console.log(gameObject());
